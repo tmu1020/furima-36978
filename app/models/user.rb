23cdeba92c50485f6cb8e946f9cf2be852f30071
tmu1020 <tmu1020@gmail.com>
2022-01-27 21:39:11 +0900

    エラー文・バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+
+  with_options format: { with: /\A[a-zA-Z0-9]+\z/, message: 'is invalid. Include both letters and numbers' } do
+    validates :password
+  end
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+    
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birth, presence: true
 end
