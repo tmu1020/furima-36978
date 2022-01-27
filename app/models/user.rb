51a4ebe80af07780ab9e44d7f4cac1546c19b167
tmu1020 <tmu1020@gmail.com>
2022-01-27 21:44:29 +0900

    バリデーション整頓

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
+  validates :birth, presence: true
 
   with_options format: { with: /\A[a-zA-Z0-9]+\z/, message: 'is invalid. Include both letters and numbers' } do
     validates :password
   end
-  
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
-    validates :first_name
     validates :last_name
+    validates :first_name
   end
     
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :last_name_kana
     validates :first_name_kana
   end
-
-  validates :birth, presence: true
 end
