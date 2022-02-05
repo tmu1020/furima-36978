8d3f67dd81e4feea1252394f992314bc5398928c
tmu1020 <tmu1020@gmail.com>
2022-02-05 17:49:09 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image, :item_name, :description, :category_id, :condition_id,
+      :shipping_cost_id, :prefecture_id, :shipping_day_id, :price
+    ).merge(
+      user_id: current_user.id)
   end
 end
