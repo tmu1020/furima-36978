29194f8694ff4076982cafd364ae59c297e5559c
tmu1020 <tmu1020@gmail.com>
2022-02-11 15:34:57 +0900

    細かい修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,33 +4,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :purchase_record
 
-  # Active Hashとのアソシエーション
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_cost
-  belongs_to :shipping_day
-  belongs_to :prefecture
-
-  # Active Storageとのアソシエーション
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :prefecture
+  
   has_one_attached :image
 
   with_options presence: true do
-    validates :user_id
     validates :image
     validates :item_name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :shipping_cost_id
-    validates :shipping_day_id
-    validates :prefecture_id
-    # 300円以上、9,999,999円以下、半角数字で保存可能
-    validates :price, numericality: {
-      greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
-    }
+    validates :price
   end
-
-  # 選択が「--」の時は保存不可
+  # 選択が「---」の時は保存不可
   with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
@@ -38,4 +26,7 @@ class Item < ApplicationRecord
     validates :shipping_day_id
     validates :prefecture_id
   end
+  # 半角数字かつ、¥300〜¥9,999,999の間で保存可能
+  validates :price, numericality: { message: "is invalid. Input half-width characters" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
-      it 'nickname、email、password、password_confirmation、
-          last_name、first_name、last_name_kana、first_name_kana、birthday
-          が存在すれば登録できる' do
+      it '全ての入力事項が存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
