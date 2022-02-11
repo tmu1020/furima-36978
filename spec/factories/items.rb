04b54fcff20da2eaadf2d0c11c00eadfba5fc566
tmu1020 <tmu1020@gmail.com>
2022-02-11 15:37:26 +0900

    itemモデルの単体テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,18 @@
 FactoryBot.define do
   factory :item do
+    association :user
+
+    item_name        {'test-item'}
+    description      {'test'}
+    category_id      { 1 }
+    condition_id     { 1 }
+    shipping_cost_id { 1 }
+    prefecture_id    { 1 }
+    shipping_day_id  { 1 }
+    price            { 300 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
