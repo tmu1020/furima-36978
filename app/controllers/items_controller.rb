9aa0d9cbf9e69d41edad8344631c2a23d1453aa3
tmu1020 <tmu1020@gmail.com>
2022-02-12 23:53:34 +0900

    edit, updateアクションの定義
    ・ログインユーザーと投稿者が一致しなければ、トップページに戻る設定
    ・売却済み商品に対してedit, updateしようとすると、トップページに戻る設定をし、コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
+  # before_action :purchase_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,7 +22,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id), method: :get
+    else
+      render :edit
+    end
   end
 
   private
@@ -31,5 +44,19 @@ class ItemsController < ApplicationController
     ).merge(
       user_id: current_user.id
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+
+  def purchase_confirmation
+    # if @item.purchase_record.present?
+    # redirect_to root_path
+    # end
   end
 end
