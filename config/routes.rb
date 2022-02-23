7554d05261f433fedbc0d4bb76ac4c416fc37f75
tmu1020 <tmu1020@gmail.com>
2022-02-23 17:39:24 +0900

    purchase_recordsをitemsにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :purchase_records, only: [:index, :create]
+  resources :items do
+    resources :purchase_records, only: [:index, :create]
+  end
 end
