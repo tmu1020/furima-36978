c8055c41830667203e94335f0045eac10ef6ef6b
tmu1020 <tmu1020@gmail.com>
2022-02-28 22:23:51 +0900

    決済機能の実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,9 +1,9 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:id])
-    if user_signed_in? && current_user.id != @item.user_id && @item.purchase_record == nil
+    if user_signed_in? && current_user.id != @item.user_id && @item.purchase_record.nil?
       @purchase_address = PurchaseAddress.new
     else
       redirect_to root_path
@@ -11,21 +11,36 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
-    @purchase_address = PurchaseAddress.new(purchase_record_params)
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
-      # pay_item
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
       render :index
+    end
   end
-end
 
-private
+  private
 
-def purchase_record_params
-  params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number
-  ).merge(
-    item_id: params[:item_id],user_id: current_user.id
-  )
+  def purchase_address_params
+    params.require(:purchase_address).permit(
+      :post_code, :prefecture_id, :city, :address, :building_name, :phone_number
+    ).merge(
+      item_id: params[:item_id], user_id: current_user.id, token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_address_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
